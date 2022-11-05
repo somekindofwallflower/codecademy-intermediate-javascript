fc798f3a4a67b5133796652a69265475bc342489
somekindofwallflower <someone.ana13@gmail.com>
2022-11-05 17:18:57 +0100

    Did 02-Classes / 1-Classes / 03-Instance

---
+++ 02-Classes/1-Classes/03-Instance/main.js
@@ -0,0 +1,24 @@
+class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
+
+/*
+Create an instance of the Surgeon class — set the name to 'Francisco Romero' and department to 'Cardiovascular'.
+
+Save the instance to a constant variable called surgeonRomero.
+*/
+
+
+const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+
+
+/*
+Create an instance of the Surgeon class — set the name to 'Ruth Jackson' and department to 'Orthopedics'.
+
+Save the instance to a constant variable called surgeonJackson.
+*/
+
+const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
