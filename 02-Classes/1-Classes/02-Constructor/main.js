a39c0402e56494a5148b12c2ce3776c913e72b7a
somekindofwallflower <someone.ana13@gmail.com>
2022-11-05 17:17:51 +0100

    Did 02-Classes / 1-Classes / 02-Constructor

---
+++ 02-Classes/1-Classes/02-Constructor/main.js
@@ -0,0 +1,16 @@
+/* 
+1- Create an empty class named Surgeon.
+
+2- Inside the Surgeon class, create a constructor() method that accepts two parameters: name and department.
+
+3- Inside the Surgeon constructor(), create name and department properties and set them equal to your input parameters.
+*/
+
+
+
+class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
